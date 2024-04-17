63a2155ade0fe3590bbded623bca9d78bf63b509
Nat Ersoz <natersoz@gmail.com>
2024-04-16 21:49:37 -0700

    binary_tree using CMake

--- binary_tree/test_binary_tree.cc
+++ binary_tree/test_binary_tree.cc
@@ -2,8 +2,8 @@
  * @file test_binary_tree.cc
  */
 
-#include "binary_tree.h"
-#include "utility/make_array.h"
+// #include "binary_tree.h"
+// #include "utility/make_array.h"
 
 #include <array>
 #include <numeric>
@@ -11,8 +11,14 @@
 #include <iomanip>
 #include <cassert>
 
-#if 1   // Just print the integers
+#include "gtest/gtest.h"
 
+#include "binary_tree/binary_tree.h"
+
+using namespace testing;
+
+
+// print the binary tree test_values
 template<typename Type>
 std::ostream& operator<<(std::ostream &os, binary_tree<Type> const &tree)
 {
@@ -25,40 +31,6 @@ std::ostream& operator<<(std::ostream &os, binary_tree<Type> const &tree)
     return os;
 }
 
-#else   // Full debug printing
-
-// template<typename Type>
-std::ostream& operator<<(std::ostream &os, typename binary_tree<int>::tree_node const& node)
-{
-    os << " node: " << std::hex << std::setw(8) << std::setfill('0')
-       << &node;
-    os << " parent: " << std::hex << std::setw(8) << std::setfill('0')
-       << node.parent;
-    os << " left:   " << std::hex << std::setw(8) << std::setfill('0')
-       << node.left;
-    os << " right:  " << std::hex << std::setw(8) << std::setfill('0')
-       << node.right;
-    os << " data:   " << std::dec << std::setw(4) << std::setfill(' ')
-       << node.data;
-    return os;
-}
-
-// template<typename Type>
-std::ostream& operator<<(std::ostream &os, binary_tree<int> const &tree)
-{
-    os << "head: " ;
-    os << *tree.head_node << std::endl;
-
-    for (auto iter = tree.begin(); iter != tree.end(); ++iter)
-    {
-        os << "iter: " << *iter.pos << std::endl;
-    }
-
-    return os;
-}
-#endif
-
-
 template<typename Type>
 void check_ascending_forward(binary_tree<Type> &tree)
 {
@@ -68,7 +40,7 @@ void check_ascending_forward(binary_tree<Type> &tree)
     {
         int const value_1 = *tree_iter++;
         int const value_2 = *tree_iter;
-        assert(value_2 > value_1);
+        EXPECT_GT(value_2, value_1);
     }
 }
 
@@ -83,81 +55,153 @@ void check_descending_reverse(binary_tree<Type> &tree)
         int const value_2 = *tree_iter;
         // std::cout << std::dec << std::setw(4) << std::setfill(' ') << value_1 << ", ";
         // std::cout << std::dec << std::setw(4) << std::setfill(' ') << value_2 << std::endl;
-        assert(value_2 < value_1);
+        EXPECT_LT(value_2, value_1);
     }
 }
 
-int main()
+class TestBinaryTree : public Test
 {
-    int result = 0;
-
-    binary_tree<int> tree_1;
-    assert(tree_1.empty());
-
+public:
     // Test data:
-    std::array<int, 19u> values = {
+    static constexpr std::array<int, 19u> const test_values = {
     //    1    2    3    4    5    6    7    8    9   10
         100,  50, 200,  25,  75,  10,  60, 150, 140, 130,
         135, 300, 400, 350, 275, 375, 380, 385,  80
     };
 
-    std::array<binary_tree<int>::tree_node, values.size()> tree_nodes =
-        make_initialized_array<binary_tree<int>::tree_node>(values);
+    // Values that are not to be found within the test_data.
+    // Used to check that these cannot be found within the tree.
+    static constexpr std::array<int, 3u> const invalid_values = {-1, 1001, 401};
 
-    static_assert(tree_nodes.size() == values.size());
+    std::array<binary_tree<int>::tree_node, test_values.size()> tree_nodes;
 
-    for (binary_tree<int>::tree_node& node : tree_nodes)
+    binary_tree<int> test_tree;
+
+    TestBinaryTree() : Test(), test_tree()
     {
-        tree_1.insert(node);
-    }
+        EXPECT_TRUE(test_tree.empty());
 
-    std::cout << "--- tree 1 created:" << std::endl;
-    std::cout << tree_1 << std::endl;
-
-    check_ascending_forward(tree_1);
-    check_descending_reverse(tree_1);
-
-    assert(tree_1.size() == values.size());
-    assert(not tree_1.empty());
-
-    // Check that all values can be found in the tree and
-    // that the returned iterator dereferences correctly.
-    for (int const& value : values)
-    {
-        auto iter = tree_1.find(value);
-        assert(value == *iter);
-    }
-
-    // Searching for a value that does not exist should return end().
-    {
-        auto const iter = tree_1.find(10001);
-        assert(iter == tree_1.end());
-    }
-
-    {
-        auto iter_50 = tree_1.find(50);
-        tree_1.erase(iter_50);
-        check_ascending_forward(tree_1);
-        check_descending_reverse(tree_1);
-
-        auto iter_80 = tree_1.find(80);
-        tree_1.erase(iter_80);
-        check_ascending_forward(tree_1);
-        check_descending_reverse(tree_1);
-
-        while(tree_1.size() > 0u)
+        // Check that no values in the invalid_values array are found in the test_values array.
+        for (auto invalid_value : invalid_values)
         {
-            assert(not tree_1.empty());
-            auto iter = tree_1.begin();
-            tree_1.erase(iter);
-            check_ascending_forward(tree_1);
-            check_descending_reverse(tree_1);
+            EXPECT_EQ(
+                std::find(test_values.begin(), test_values.end(), invalid_value),
+                test_values.end());
         }
     }
 
-    tree_1.clear();
-    assert(tree_1.empty());
-    assert(tree_1.size() == 0u);
+    void fill_test_data()
+    {
+        auto value_iter = test_values.begin();
+        for (binary_tree<int>::tree_node& node : tree_nodes)
+        {
+            node.data = *value_iter++;
+            this->test_tree.insert(node);
+        }
 
-    return result;
+        // std::cout << "--- tree 1 created:" << std::endl;
+        // std::cout << test_tree << std::endl;
+    }
+};
+
+TEST_F(TestBinaryTree, check_filled_test_data)
+{
+    EXPECT_TRUE(test_tree.empty());
+    EXPECT_EQ(test_tree.size(), 0u);
+
+    fill_test_data();
+
+    check_ascending_forward(test_tree);
+    check_descending_reverse(test_tree);
+
+    EXPECT_EQ(test_tree.size(), test_values.size());
+    EXPECT_FALSE(test_tree.empty());
+
+    // Check that all test_values can be found in the tree and
+    // that the returned iterator dereferences correctly.
+    for (int const& value : test_values)
+    {
+        auto iter = test_tree.find(value);
+        EXPECT_EQ(value, *iter);
+    }
+
+    // Check that invalid values are not found within the binary tree.
+    for (int const value : invalid_values)
+    {
+        auto iter = test_tree.find(value);
+        EXPECT_EQ(test_tree.end(), iter);
+    }
+}
+
+TEST_F(TestBinaryTree, check_test_data_erasures)
+{
+    EXPECT_TRUE(test_tree.empty());
+    EXPECT_EQ(test_tree.size(), 0u);
+
+    fill_test_data();
+
+    check_ascending_forward(test_tree);
+    check_descending_reverse(test_tree);
+
+    EXPECT_EQ(test_tree.size(), test_values.size());
+    EXPECT_FALSE(test_tree.empty());
+
+    // Check that all test_values can be found in the tree and
+    // that the returned iterator dereferences correctly.
+    for (int const& value : test_values)
+    {
+        auto iter = test_tree.find(value);
+        EXPECT_EQ(value, *iter);
+    }
+
+    auto iter_50 = test_tree.find(50);
+    test_tree.erase(iter_50);
+    auto iter_50_bad = test_tree.find(50);
+    EXPECT_EQ(iter_50_bad, test_tree.end());
+    check_ascending_forward(test_tree);
+    check_descending_reverse(test_tree);
+
+    auto iter_80 = test_tree.find(80);
+    test_tree.erase(iter_80);
+    auto iter_80_bad = test_tree.find(80);
+    EXPECT_EQ(iter_80_bad, test_tree.end());
+    check_ascending_forward(test_tree);
+    check_descending_reverse(test_tree);
+
+    while(test_tree.size() > 0u)
+    {
+        EXPECT_FALSE(test_tree.empty());
+        auto iter = test_tree.begin();
+        test_tree.erase(iter);
+        auto iter_bad = test_tree.find(80);
+        EXPECT_EQ(iter_bad, test_tree.end());
+        check_ascending_forward(test_tree);
+        check_descending_reverse(test_tree);
+    }
+}
+
+TEST_F(TestBinaryTree, check_test_data_clear)
+{
+    EXPECT_TRUE(test_tree.empty());
+    EXPECT_EQ(test_tree.size(), 0u);
+
+    fill_test_data();
+
+    check_ascending_forward(test_tree);
+    check_descending_reverse(test_tree);
+
+    EXPECT_EQ(test_tree.size(), test_values.size());
+    EXPECT_FALSE(test_tree.empty());
+
+    // Check that all test_values can be found in the tree and
+    // that the returned iterator dereferences correctly.
+    for (int const& value : test_values)
+    {
+        auto iter = test_tree.find(value);
+        EXPECT_EQ(value, *iter);
+    }
+
+    test_tree.clear();
+    EXPECT_TRUE(test_tree.empty());
+    EXPECT_EQ(test_tree.size(), 0u);
 }
